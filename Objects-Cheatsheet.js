8985806434a829ca5093620e9e1f862eb1dcc3db
KumihoBG <you@example.com>
2021-03-02 22:09:13 +0200

    commit

--- Objects-Cheatsheet.js
+++ Objects-Cheatsheet.js
@@ -195,3 +195,49 @@ function solve(input) {
         }
     }
 }
+
+// Party time guests list
+function partyTime(input) {
+    let index = input.indexOf("PARTY");
+    let guestList = input.slice(0, index);
+    let guestsArrived = input.slice(index + 1);
+    let reservations = generateList(guestList);
+
+    guestsArrived.forEach(guest => {
+        let status = checkVip(guest);
+        let correctList = status ? "vip" : "regular";
+        let currentGuest = reservations[correctList].find(o => o.guest == guest);
+        if (status && currentGuest){
+            currentGuest.isHere = true;
+        } else if (!status && currentGuest){
+            currentGuest.isHere = true;
+        }
+    });
+
+    let vips = reservations.vip.filter(o => o.isHere === false);
+    let regulars = reservations.regular.filter(o => o.isHere === false);
+    console.log(vips.length + regulars.length);
+    vips.concat(regulars).forEach(el => console.log(el.guest));
+
+    function generateList(guests) {
+        let obj = {
+            vip: [],
+            regular: [],
+        };
+
+        guests.forEach((guest) => {
+            let guestObj = { guest, isHere: false }
+            if (checkVip(guest)) {
+                obj.vip.push(guestObj);
+            } else {
+                obj.regular.push(guestObj);
+            }
+        });
+
+        return obj;
+    }
+
+    function checkVip(guest) {
+        return !isNaN(Number(guest[0]));
+    }
+}
