48df65198d1cfddad2c8177e8c21dc147383154b
KumihoBG <you@example.com>
2021-03-03 18:01:07 +0200

    commit

--- Objects-Cheatsheet.js
+++ Objects-Cheatsheet.js
@@ -241,3 +241,61 @@ function partyTime(input) {
         return !isNaN(Number(guest[0]));
     }
 }
+
+// Card game - set, split, unique arrays in an object
+function cardGame(input) {
+    let players = generatePlayers(input);
+    let cardMapper = {
+        p: {
+            "J": 11,
+            "Q": 12,
+            "K": 13,
+            "A": 14,
+        },
+        t: {
+            "S": 4,
+            "H": 3,
+            "D": 2,
+            "C": 1,
+        }
+    }
+
+    Object.keys(players).forEach(player => {
+        console.log(`${player}: ${totalPlayerResult(players[player])}`);
+    });
+
+    function totalPlayerResult(set) {
+        let totalPower = 0;
+        set.forEach(card => {
+            let currentCard = card.split('');
+            let value = currentCard.slice(0, currentCard.length - 1).join('');
+            let suit = currentCard.slice(-1).join('');
+            let p = 0;
+        
+            if (!cardMapper.p[value]) {
+                p = Number(value);
+            } else {
+                p = cardMapper.p[value];
+            }
+
+            totalPower += (p * cardMapper.t[suit]);
+        })
+        return totalPower;
+    }
+
+    function generatePlayers(arr) {
+        let obj = {};
+        arr.forEach(playerInfo => {
+            let [playerName, playerCards] = playerInfo.split(': ');
+            let currentCardsList = playerCards.split(', ');
+            if (!obj[playerName]) {
+                obj[playerName] = new Set(currentCardsList);
+            } else {
+               playerCards.split(', ').forEach(card => {
+                   obj[playerName].add(card);
+               });
+            }
+        });
+        return obj;
+    }
+}
