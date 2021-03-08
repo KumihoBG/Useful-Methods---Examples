69f1b9c4c0146329cdccaa141238fb4a51b46f47
KumihoBG <you@example.com>
2021-03-08 10:53:58 +0200

    commit

--- Objects-Cheatsheet.js
+++ Objects-Cheatsheet.js
@@ -377,3 +377,27 @@ function solve(input) {
       console.log(`${key} -> ${value}`);
     }
   }
+
+
+  // Flights - check status - create object with object property
+  function flightSchedule(input) {
+    let flights = {};
+    input[0].forEach(line => {
+        let [numberFly, destination] = line.split(' ')
+        flights[numberFly] = {
+            Destination: destination,
+            Status: 'Ready to fly'
+        }
+    });
+    input[1].forEach(line => {
+        let [numberFly, status] = line.split(' ');
+        if (flights.hasOwnProperty(numberFly)) {
+            flights[numberFly].Status = status;
+        }
+    });
+    for (let fly in flights) {
+        if (flights[fly].Status === String(input[2])) {
+            console.log(flights[fly]);
+        }
+    }
+}
