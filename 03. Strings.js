fd014c633e6d90d505150b4d7a2fbe699064a565
KumihoBG <you@example.com>
2021-03-10 23:15:34 +0200

    commit

--- 03. Strings.js
+++ 03. Strings.js
@@ -5,4 +5,32 @@ function countStrings(text, word){
     .filter(el => el == word).length);
 }
 
+// Finding all positions (indexes) of a word in a sentence
+function find(word){
+    let currentIndex = word.indexOf("Java");
+
+    while (currentIndex !== -1) {
+        currentIndex = word.indexOf("Java", currentIndex + 1);
+        return currentIndex;
+    }
+}
+
+// Slice string - substring(startIndex, endIndex)
+// endIndex not included and if it -negative, it automatically becomes 1st index=0 and startIndex becomes endIndex
+function subString(string, startIndex, count) {
+    startIndex = Number(startIndex);
+    count = Number(count);
+    return string.substring(startIndex, startIndex + count);
+}
+
+// Replace - replace(search, replacement)
+function solve(text, word) {
+    let indexOfMatch = text.indexOf(word);
+
+    while (indexOfMatch !== -1) {
+        text = text.replace(word, "*" .repeat(word.length));
+        indexOfMatch = text.indexOf(word, indexOfMatch + 1);
+    }
+    console.log(text);
+}
 
