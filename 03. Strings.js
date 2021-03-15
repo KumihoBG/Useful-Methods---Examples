224ac853b8d36c5f5f1cf53393f324c99db634d6
KumihoBG <you@example.com>
2021-03-15 09:55:32 +0200

    commit

--- 03. Strings.js
+++ 03. Strings.js
@@ -495,3 +495,49 @@ function passGenerator(str) {
         return m === null ? 0 : m.length;
     }
 }
+
+// sum nums inside strings
+function lettersChange(input) {
+    // take input and filter for more than one empty space for split
+    let pairs = input.split(' ').filter(w => w.length > 0);
+    let result = 0;
+    let totalResult = 0;
+
+    for (let line of pairs) {
+        line = Array.from(line);
+        let first = line.shift();
+        let second = line.pop();
+        line = line.join('');
+        line = Number(line);
+        let position = Number(alphabetPosition(first));
+        let positionTwo = Number(alphabetPosition(second))
+        
+        if (first === first.toUpperCase() || first === first.toUpperCase()) {  
+            result = line / position;
+        } else {
+            result = line * position;
+        }
+    
+        if (second === second.toUpperCase() || second === second.toUpperCase()) {  
+            result -= positionTwo;
+        } else {
+            result += positionTwo;
+        }
+
+        totalResult += result;
+    }
+
+    console.log(totalResult.toFixed(2));
+
+    // find alphabet position of a letter
+    function alphabetPosition(text) {
+        let result = "";
+        for (let i = 0; i < text.length; i++) {
+            let code = text.toUpperCase().charCodeAt(i)
+            if (code > 64 && code < 91) {
+                result += (code - 64) + " ";
+            }
+        }
+        return result.slice(0, result.length - 1);
+    }
+}
