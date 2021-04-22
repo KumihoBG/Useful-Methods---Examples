d1dde06467ffa715c63fe36a004226684ef44ff1
Kumiho <pavleta.taseva@gmail.com>
2021-04-22 10:43:36 +0300

    commit

--- 00. DOM.js
+++ 00. DOM.js
@@ -7,3 +7,37 @@ function edit(ref, match, replacer) {
     // set the new content found via result
     ref.textContent = result;
 }
+
+// using regex
+function extract(id) {
+    const text = document.getElementById(id).textContent;
+    const regex = /\((.+?)\)/g;
+    let result = [];
+    let match = regex.exec(text);
+
+    while (match != null) {
+        result.push(match[1]);
+        match = regex.exec(text);
+    }
+
+    return result.join('; ');
+}
+
+// sum elements in dom
+function sumTable() {
+    const rows = [...document.querySelectorAll('table tr')].slice(1, -1);
+    document.getElementById('sum').textContent = rows.reduce((sum, row) => {
+        return sum + Number(row.lastElementChild.textContent);
+    }, 0);
+}
+
+// colorize table
+function colorize() {
+    [...document.querySelectorAll('table tr:nth-child(even)')].forEach(el => el.style.backgroundColor = 'teal');
+}
+
+// show more content
+function showText() {
+    document.getElementById('more').style.display = 'none'
+    document.getElementById('text').style.display = 'inline';
+}
