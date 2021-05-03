e09d2d3305dcac4bdc9fc317e325fe150d9bc115
Kumiho <pavleta.taseva@gmail.com>
2021-05-03 16:08:49 +0300

    commit

--- 00. DOM.js
+++ 00. DOM.js
@@ -233,3 +233,20 @@ function deleteByEmail() {
 
  resultElement.textContent = 'Not found.'
 }
+
+// Mouse hover percent of DIV
+function attachGradientEvents() {
+  let gradient = document.getElementById('gradient');
+  gradient.addEventListener('mousemove', gradientMove);
+  gradient.addEventListener('mouseout', gradientOut);
+  
+  function gradientMove(event) {
+      let power = event.offsetX / (event.target.clientWidth - 1);
+      power = Math.trunc(power * 100);
+      document.getElementById('result').textContent = power + "%";
+  }
+  
+  function gradientOut(event) {
+      document.getElementById('result').textContent = "";
+  }
+}
