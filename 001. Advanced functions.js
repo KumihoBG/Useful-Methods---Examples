1eb116942278659b4eff43d0ecfa78ffc897f7e6
Kumiho <pavleta.taseva@gmail.com>
2021-05-19 11:13:58 +0300

    commit

---
+++ 001. Advanced functions.js
@@ -0,0 +1,23 @@
+// Area and volume calculator
+function area() {
+    return Math.abs(this.x * this.y);
+};
+
+function vol() {
+    return Math.abs(this.x * this.y * this.z);
+};
+
+// Input as two constructor functions and array of objects
+function solve(area, vol, input) {
+    // Defining the figure
+    const figures = JSON.parse(input);
+    //Using map to loop through the coordinates of the figure
+    const result = figures.map(figure => ({
+        // The call() method calls a function with a given this value and arguments provided individually.
+        area: Math.abs(area.call(figure)),
+        // The apply() method calls a function with a given this value, and arguments provided as an array (or an array-like object).
+        volume: Math.abs(vol.apply(figure))
+    }));
+
+    return result;
+}
