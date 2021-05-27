b9f87633c6072dfa7f80ddec846a9424184e775c
Kumiho <pavleta.taseva@gmail.com>
2021-05-27 08:59:16 +0300

    commit

---
+++ 002. Errors & Unit testing.js
@@ -0,0 +1,42 @@
+// Sum numbers
+function subSum(arr, start, end) {
+    // check if it is an array
+    if (Array.isArray(arr) == false) {
+        return NaN;
+    }
+
+    if (start < 0) {
+        start = 0;
+    }
+
+    if (end > arr.length - 1) {
+        end = arr.length - 1;
+    }
+    return arr.slice(start, end + 1).reduce((a, b) => a + Number(b), 0);
+}
+
+// Playing cards
+function createCard(face, suit) {
+    const validFaces = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'A', 'K'];
+    const suitToString = {
+        'S': '\u2660',
+        'H': '\u2665',
+        'D': '\u2666',
+        'C': '\u2663',
+    };
+
+    // Throw errors
+    if (validFaces.includes(face) == false) {
+        throw new Error('Invalid face');
+    } else if (Object.keys(suitToString).includes(suit) == false) {
+        throw new Error('Invalid suit');
+    };
+
+    return {
+        face,
+        suit,
+        toString() {
+            return `${face}${suitToString[suit]}`;
+        }
+    };
+}
