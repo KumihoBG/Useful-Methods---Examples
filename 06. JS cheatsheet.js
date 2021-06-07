649974b956386992153db1f38a31ea80489b2e1f
Kumiho <pavleta.taseva@gmail.com>
2021-06-07 11:39:13 +0300

    commit

--- 06. JS cheatsheet.js
+++ 06. JS cheatsheet.js
@@ -144,6 +144,23 @@ function area(arg1, arg2, arg3) {
     }
 }
 
+// class Circle
+class Circle {
+    constructor(radius) {
+        this.radius = radius;
+    }
+
+    get area() {
+        return Math.PI * (this.radius **= 2);
+    }
+    get diameter() {
+        return this.radius * 2;
+    }
+    set diameter(x) {
+        this.radius = x / 2;
+    }
+}
+
 // Cone area and volume
 function cone(radius, height){
     let slantHeight = Math.sqrt(radius * radius + height * height);
