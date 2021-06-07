029344519580313df3cf9aa5b62b059c6a90195b
Kumiho <pavleta.taseva@gmail.com>
2021-06-07 11:46:55 +0300

    commit

--- 06. JS cheatsheet.js
+++ 06. JS cheatsheet.js
@@ -788,6 +788,15 @@ function magicNum(input, magicNum){
 
     }
 }
+// Class point
+class Point {    
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    static distance = (p1, p2) => { return Math.sqrt((p1.x - p2.x) ** 2 + (p1.y - p2.y) ** 2);}
+}
 
 // Get distance between 2 points x and y
 function distance(x1, y1, x2, y2) {
