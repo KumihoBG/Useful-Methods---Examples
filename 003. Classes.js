281c22e739c86c8d5bda345c92598cbffc19d4e3
Kumiho <pavleta.taseva@gmail.com>
2021-06-21 11:36:53 +0300

    commit

--- 003. Classes.js
+++ 003. Classes.js
@@ -250,3 +250,64 @@ class ChristmasDinner {
         .join('\n');
     }
 }
+
+// Common task - find, findIndex
+class Parking {
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.vehicles = [];
+    }
+
+    addCar(carModel, carNumber) {
+        if (this.capacity === 0) {
+            throw new Error("Not enough parking space.");
+        }
+        this.vehicles.push({ carModel, carNumber, payed: false});
+        this.capacity--;
+        return `The ${carModel}, with a registration number ${carNumber}, parked.`
+    }
+
+    // Looking for a string in an object value, nested in an array
+    removeCar(carNumber) {
+        let carIndex = this.vehicles.findIndex(car => car.carNumber === carNumber);
+        if (carIndex < 0) {
+            throw new Error(`The car, you're looking for, is not found.`);
+        }
+        // Accessing the property inside the array
+        if (!this.vehicles[carIndex].payed) {
+            throw new Error(`${carNumber} needs to pay before leaving the parking lot.`);
+        }
+
+        this.vehicles.splice(carIndex, 1);
+        this.capacity++;
+        return `${carNumber} left the parking lot.`;
+    }
+
+    // Looking for a string in an object value, nested in an array
+    pay(carNumber) {
+        let car = this.vehicles.find(car => car.carNumber === carNumber);
+        if (!car) {
+            throw new Error(`${carNumber} is not in the parking lot.`);
+        }
+        if (car.payed) {
+            throw new Error(`${carNumber}'s driver has already payed his ticket.`);
+        }
+        car.payed = true;
+        return `${carNumber}'s driver successfully payed for his stay.`;
+    }
+
+    getStatistics(carNumber) {
+        if (!carNumber) {
+            return [
+                `The Parking Lot has ${this.capacity} empty spots left.`,
+                this.vehicles
+                .sort((a, b) => a.carModel.localeCompare(b.carModel))
+                .map(car => 
+                    `${car.carModel} == ${car.carNumber} - ${car.payed ? 'Has payed': 'Not payed'}`)
+                .join('\n')
+                ].join('\n');
+        }
+        let car = this.vehicles.find(car => car.carNumber === carNumber);
+        return `${car.carModel} == ${car.carNumber} - ${car.payed ? 'Has payed': 'Not payed'}`;
+    } 
+}
